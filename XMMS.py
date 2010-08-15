80f4706693bf8f881be7d1c2dae19539ab4d07b7
Ben Laurie <ben@links.org>
2010-08-15 17:20:52 +0100

    Add shuffle methods.

--- XMMS.py
+++ XMMS.py
@@ -6,3 +6,10 @@ class XMMS:
     def getCurrentTrackPath(self):
         return xmms.control.get_playlist_file(xmms.control.get_playlist_pos())
 
+    def getShuffle(self):
+        # is_shuffle() returns 0 or 1 instead of true or false
+        return not not xmms.control.is_shuffle()
+
+    def setShuffle(self, status):
+        if not status is self.getShuffle():
+            xmms.control.toggle_shuffle()
