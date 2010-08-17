fd185ea7ea90551ec3f83d33c95d575a24160680
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2010-08-17 03:10:02 +0100

    Added error handling to Logger.

--- Logger.py
+++ Logger.py
@@ -1,3 +1,5 @@
+## Logger
+
 import logging
 import datetime
 
@@ -11,7 +13,7 @@ class LoggerFactory:
         errorLogFilename = "logs/error_"+dateString+".log"
 
         formatter = logging.Formatter(
-            "%(asctime)s   %(name)-12s %(levelname)-8s %(message)s",
+            "%(asctime)s   %(name)-13s %(levelname)-8s %(message)s",
             "%Y-%m-%d %H:%M:%S")
 
         commandLineHandler = logging.StreamHandler()
@@ -30,14 +32,16 @@ class LoggerFactory:
         logging.getLogger("NQr").addHandler(debugFileHandler)
         logging.getLogger("NQr").addHandler(errorFileHandler)
 
+        self._logger = logging.getLogger("NQr.Logger")
+        self._logger.setLevel(logging.DEBUG)
+
     def getLogger(self, name, level):
         logger = logging.getLogger(name)
         if level == "debug":
             logger.setLevel(logging.DEBUG)
-        if level == "error":
+        elif level == "error":
             logger.setLevel(logging.ERROR)
+        else:
+            self._logger.error("Invalid level set for logger.")
+            raise ValueError(str(level)+" is an invalid level for logger.")
         return logger
-##        GUILogger = logging.getLogger("NQr.GUI")
-##        GUILogger.setLevel(logging.DEBUG)
-##
-##        GUILogger.error("test")
