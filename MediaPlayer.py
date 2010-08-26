9f635990d9de75ccfb15fda912746ccf486690e5
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2010-08-26 03:37:39 +0100

    Added logging to MediaPlayer.

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -2,6 +2,7 @@
 
 class MediaPlayer:
     def savePlaylist(self):
+        self._logger.debug("Storing current playlist.")
         playlist = []
         for trackPosition in range(self.getPlaylistLength()):
             playlist.append(self.getTrackPathAtPos(trackPosition))
@@ -10,6 +11,7 @@ class MediaPlayer:
 ## FIXME: sets currently playing track to first track in the list, but continues
 ##        to play the old track (fixed with work-around)
     def loadPlaylist(self, playlist):
+        self._logger.debug("Restoring playlist.")
         currentTrackPath = self.getCurrentTrackPath()
         self.clearPlaylist()
         self.addTrack(currentTrackPath)
@@ -17,11 +19,15 @@ class MediaPlayer:
             self.addTrack(filepath)
 
     def cropPlaylist(self, number):
+        if number == 1:
+            self._logger.debug("Cropping playlist by "+str(number)+" track.")
+        else:
+            self._logger.debug("Cropping playlist by "+str(number)+" tracks.")
         for n in range(number):
             self.deleteTrack(0)
 
 ## FIXME: gets confused if the playlist is empty (in winamp): sets currently
 ##        playing track to first track in the list, but continues to play the
 ##        old track
-    def getCurrentTrackPath(self, warnings=True):
-        return self.getTrackPathAtPos(self.getCurrentTrackPos(), warnings)
+    def getCurrentTrackPath(self, logging=True):
+        return self.getTrackPathAtPos(self.getCurrentTrackPos(), logging)
