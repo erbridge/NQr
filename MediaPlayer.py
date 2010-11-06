e5bc3f3b7c40b1c120e4542cbf9576e1d91cbdb5
Ben Laurie <ben@links.org>
2010-11-06 16:05:13 +0000

    Use logger for "not queuing" messags

--- MediaPlayer.py
+++ MediaPlayer.py
@@ -59,13 +59,13 @@ class MediaPlayer:
 
     def addTrack(self, filepath):
         if self._noQueue:
-            print "Not queueing", filepath
+            self._logger.info("Not queueing " + filepath)
             return
         self._addTrack(filepath)
         
     def insertTrack(self, filepath, position):
         if self._noQueue:
-            print "Not queueing", filepath
+            self._logger.info("Not queueing " + filepath)
             return
         self._insertTrack(filepath, position)
 
