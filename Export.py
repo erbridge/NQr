272d4bab431cd2b4e2010f3a7cd5b1c236bdacb4
Ben Laurie <ben@links.org>
2010-12-18 14:03:18 +0000

    Use new column for score.

--- Export.py
+++ Export.py
@@ -7,12 +7,14 @@ def main():
     # select trackid, score, max(scoreid) from scores group by trackid;
 #    cursor.execute("""select trackid, score from scores
 #                      group by trackid order by scoreid""")
-    cursor.execute("""select scores.trackid, score, path from scores, tracks
-                      where scores.trackid = tracks.trackid
-                      group by scores.trackid order by scoreid""")
+#    cursor.execute("""select scores.trackid, score, path from scores, tracks
+#                      where scores.trackid = tracks.trackid
+#                      group by scores.trackid order by scoreid""")
+    cursor.execute("""select score, path from tracks
+                      where score is not null""")
     results = cursor.fetchall()
     for result in results:
-        print str(result[1]) + "\t" + result[2]
+        print str(result[0]) + "\t" + result[1]
 
 if __name__ == '__main__':
     main()
