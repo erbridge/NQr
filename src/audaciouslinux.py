c096de5175a38066ac1cd390d1c53631d8e46c60
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-09-29 22:32:51 +0100

    Refactored MPRIS standardized interface into its own class to be inherited.

--- src/audaciouslinux.py
+++ src/audaciouslinux.py
@@ -1,88 +1,12 @@
 # Audacious interface
 
-import dbus
-import os
-import time
-import urllib
-import urlparse
-
 import mediaplayer
 
-class Audacious(mediaplayer.MediaPlayer):
-    
+class Audacious(mediaplayer.MprisMediaPlayer):
+
     def __init__(self, loggerFactory, noQueue, configParser, defaultPlayer,
                  safePlayers, trackFactory):
-        mediaplayer.MediaPlayer.__init__(self, loggerFactory, "NQr.Audacious",
-                                         noQueue, configParser, defaultPlayer,
-                                         safePlayers, trackFactory)                
-        while True:
-            try:
-                self._initSetupDbus()
-                break
-            except dbus.exceptions.DBusException:
-                import subprocess
-                subprocess.Popen("audacious", close_fds=True)
-                time.sleep(.25)
-            
-    def _initSetupDbus(self):
-        self._bus = dbus.SessionBus()
-        
-        root = self._bus.get_object("org.mpris.audacious", "/")
-        player = self._bus.get_object("org.mpris.audacious", "/Player")
-        tracklist  = self._bus.get_object("org.mpris.audacious", "/TrackList")
-#         print player.Introspect()
-        org = self._bus.get_object("org.mpris.audacious",
-                                  "/org/atheme/audacious")
-        
-        self._root = dbus.Interface(root, "org.freedesktop.MediaPlayer")
-        self._player = dbus.Interface(player, "org.freedesktop.MediaPlayer")
-        self._tracklist = dbus.Interface(tracklist,
-                                         "org.freedesktop.MediaPlayer")
-        self._org = dbus.Interface(org, "org.atheme.audacious")
-        
-    def _addTrack(self, path):
-        path = "file://" + path
-        self._tracklist.AddTrack(path, False)
-                
-    def _getTrackPathAtPos(self, position, traceCallback=None, logging=True):
-        metadata = self._tracklist.GetMetadata(position)
-        try:
-            parsedPath = urlparse.urlparse(metadata[u"location"])
-        except KeyError:
-            # FIXME: empty tracklist throws errors
-            return None
-        return urllib.unquote(os.path.realpath(os.path.join(parsedPath.netloc,
-                                                            parsedPath.path)))
-        
-    def deleteTrack(self, position):
-        self._tracklist.DelTrack(position)
-        
-    def getCurrentTrackPos(self, traceCallback=None):
-        return self._tracklist.GetCurrentTrack()
-    
-    def getPlaylistLength(self):
-        return self._tracklist.GetLength()
-    
-    def getShuffle(self):
-        status = self._player.GetStatus()
-        return bool(status[1])
-    
-    def nextTrack(self):
-        self._player.Next()
-        self._player.Play()
-    
-    def pause(self):
-        self._player.Pause()
-    
-    def play(self):
-        self._player.Play()
-        
-    def previousTrack(self):
-        self._player.Prev()
-        self._player.Play()
-    
-    def setShuffle(self, status):
-        self._tracklist.Random(status)
-        
-    def stop(self):
-        self._player.Stop()
+        mediaplayer.MprisMediaPlayer.__init__(
+                self, loggerFactory, "NQr.Audacious", noQueue, configParser,
+                defaultPlayer, safePlayers, trackFactory, "org.mpris.audacious",
+                "audacious")
--- src/mediaplayer.py
+++ src/mediaplayer.py
@@ -4,7 +4,11 @@
 #       running and not keep reopening the player.
 
 import ConfigParser
+import dbus
 import os.path
+import time
+import urllib
+import urlparse
 
 import errors
 import util
@@ -15,7 +19,7 @@ wx = util.wx
 # FIXME: Since it now all runs in the track monitor,
 #        needs to have logs post events in classes that inherit from this one.
 class MediaPlayer(util.EventPoster):
-    
+
     def __init__(self, loggerFactory, name, noQueue, configParser,
                  defaultPlayer, safePlayers, trackFactory):
         self._logger = loggerFactory.getLogger(name, "debug")
@@ -77,9 +81,9 @@ class MediaPlayer(util.EventPoster):
         self._sendDebug(
             "Cropping playlist by " + str(number) + " track"
             + util.plural(number) + ".")
-        for n in range(number):
+        for _ in range(number):
             self.deleteTrack(0)
-        
+
     def hasNextTrack(self, traceCallback=None):
 #        try:
 #            self.getTrackPathAtPos(self.getCurrentTrackPos() + 1, traceCallback)
@@ -113,7 +117,7 @@ class MediaPlayer(util.EventPoster):
         if path is None:
             return None
         return os.path.realpath(path)
-        
+
     def getUnplayedTrackIDs(self, db, completion, traceCallback=None):
         self._ids = []
         count = 0
@@ -137,7 +141,7 @@ class MediaPlayer(util.EventPoster):
                 self._getUnplayedTrackIDListCompletion(db, count, completion,
                                                        thisCallback),
             traceCallback=traceCallback)
-        
+
     def _getUnplayedTrackIDsCallback(self, db, id, path, traceCallback):
         # The track may be one we don't know added directly to the player.
         mycompletion = lambda thisCallback, trackID: self._ids.append(trackID)
@@ -148,7 +152,7 @@ class MediaPlayer(util.EventPoster):
             traceCallback=traceCallback)
             return
         mycompletion(traceCallback, id)
-            
+
     def _getUnplayedTrackIDListCompletion(self, db, count, completion,
                                           traceCallback):
         if len(self._ids) != count:
@@ -166,7 +170,7 @@ class MediaPlayer(util.EventPoster):
             self._sendInfo("Not queueing " + filepath)
             return
         self._addTrack(filepath)
-        
+
     def insertTrack(self, filepath, position):
         if self._noQueue:
             self._sendInfo("Not queueing " + filepath)
@@ -181,102 +185,179 @@ class MediaPlayer(util.EventPoster):
         pass
 
 
+class MprisMediaPlayer(MediaPlayer):
+
+    def __init__(self, loggerFactory, name, noQueue, configParser,
+                 defaultPlayer, safePlayers, trackFactory, packageName,
+                 launchCommand):
+        MediaPlayer.__init__(self, loggerFactory, name, noQueue, configParser,
+                             defaultPlayer, safePlayers, trackFactory)
+
+        while True:
+            try:
+                self._initSetupDbus(packageName)
+                break
+            except dbus.exceptions.DBusException:
+                import subprocess
+                subprocess.Popen(launchCommand, close_fds=True)
+                time.sleep(.25)
+
+    def _initSetupDbus(self, packageName):
+        self._bus = dbus.SessionBus()
+
+        root = self._bus.get_object(packageName, "/")
+        player = self._bus.get_object(packageName, "/Player")
+        tracklist = self._bus.get_object(packageName, "/TrackList")
+
+        self._root = dbus.Interface(root, "org.freedesktop.MediaPlayer")
+        self._player = dbus.Interface(player, "org.freedesktop.MediaPlayer")
+        self._tracklist = dbus.Interface(tracklist,
+                                         "org.freedesktop.MediaPlayer")
+
+    def _addTrack(self, path):
+        path = "file://" + path
+        self._tracklist.AddTrack(path, False)
+
+    def _getTrackPathAtPos(self, position, traceCallback=None, logging=True):
+        metadata = self._tracklist.GetMetadata(position)
+        try:
+            parsedPath = urlparse.urlparse(metadata[u"location"])
+        except KeyError:
+            # FIXME: empty tracklist throws errors
+            return None
+        return urllib.unquote(os.path.realpath(os.path.join(parsedPath.netloc,
+                                                            parsedPath.path)))
+
+    def deleteTrack(self, position):
+        self._tracklist.DelTrack(position)
+
+    def getCurrentTrackPos(self, traceCallback=None):
+        return self._tracklist.GetCurrentTrack()
+
+    def getPlaylistLength(self):
+        return self._tracklist.GetLength()
+
+    def getShuffle(self):
+        status = self._player.GetStatus()
+        return bool(status[1])
+
+    def nextTrack(self):
+        self._player.Next()
+        self._player.Play()
+
+    def pause(self):
+        self._player.Pause()
+
+    def play(self):
+        self._player.Play()
+
+    def previousTrack(self):
+        self._player.Prev()
+        self._player.Play()
+
+    def setShuffle(self, status):
+        self._tracklist.Random(status)
+
+    def stop(self):
+        self._player.Stop()
+
+
 class PrefsPage(util.BasePrefsPage):
-    
+
     def __init__(self, parent, configParser, logger, defaultPlayer,
                  safePlayers):
         util.BasePrefsPage.__init__(self, parent, configParser, logger,
                                     "Player", defaultPlayer, safePlayers)
-        
+
         self._initCreatePlayerSizer()
-        
+
         mainSizer = wx.BoxSizer(wx.VERTICAL)
         mainSizer.Add(self._playerSizer, 0)
 
         self.SetSizer(mainSizer)
-    
+
     def _initCreatePlayerSizer(self):
         ID_PLAYER = wx.NewId()
-        
+
         self._playerSizer = wx.BoxSizer(wx.HORIZONTAL)
-        
+
         label = wx.StaticText(self, wx.NewId(), "Player:  \t")
-        self._playerSizer.Add(label, 0, wx.LEFT|wx.TOP|wx.BOTTOM, 3)
-        
+        self._playerSizer.Add(label, 0, wx.LEFT | wx.TOP | wx.BOTTOM, 3)
+
         if util.SYSTEM_NAME in util.WINDOWS_NAMES:
             self._winampButton = wx.RadioButton(self, ID_PLAYER, "Winamp  \t",
                                                 style=wx.RB_GROUP)
-            self._playerSizer.Add(self._winampButton, 0, wx.TOP|wx.BOTTOM, 3)
-            
+            self._playerSizer.Add(self._winampButton, 0, wx.TOP | wx.BOTTOM, 3)
+
             self._iTunesButton = wx.RadioButton(self, ID_PLAYER, "iTunes  \t")
-            self._playerSizer.Add(self._iTunesButton, 0, wx.TOP|wx.BOTTOM, 3)
-            
+            self._playerSizer.Add(self._iTunesButton, 0, wx.TOP | wx.BOTTOM, 3)
+
             if self._settings["player"] == "Winamp":
                 self._winampButton.SetValue(True)
             elif self._settings["player"] == "iTunes":
                 self._iTunesButton.SetValue(True)
-                
+
             if not util.getIsInstalled("Winamp"):
                 self._winampButton.Enable(False)
             # FIXME: May not be correct display name for iTunes (untested).
             if not util.getIsInstalled("iTunes"):
                 self._iTunesButton.Enable(False)
-                
+
         elif util.SYSTEM_NAME in util.FREEBSD_NAMES:
             self._xmmsButton = wx.RadioButton(self, ID_PLAYER, "XMMS  \t",
                                               style=wx.RB_GROUP)
-            self._playerSizer.Add(self._xmmsButton, 0, wx.TOP|wx.BOTTOM, 3)
-            
+            self._playerSizer.Add(self._xmmsButton, 0, wx.TOP | wx.BOTTOM, 3)
+
             if self._settings["player"] == "XMMS":
                 self._xmmsButton.SetValue(True)
-                
+
             if not util.getIsInstalled("XMMS"):
                 self._xmmsButton.Enable(False)
-                
+
         elif util.SYSTEM_NAME in util.MAC_NAMES:
             self._iTunesButton = wx.RadioButton(self, ID_PLAYER, "iTunes  \t",
                                                 style=wx.RB_GROUP)
-            self._playerSizer.Add(self._iTunesButton, 0, wx.TOP|wx.BOTTOM, 3)
+            self._playerSizer.Add(self._iTunesButton, 0, wx.TOP | wx.BOTTOM, 3)
 
             if self._settings["player"] == "iTunes":
                 self._iTunesButton.SetValue(True)
-                
+
             if not util.getIsInstalled("iTunes"):
                 self._iTunesButton.Enable(False)
-                
+
         elif util.SYSTEM_NAME in util.LINUX_NAMES:
             self._audaciousButton = wx.RadioButton(self, ID_PLAYER,
                                                    "Audacious  \t",
                                                    style=wx.RB_GROUP)
-            self._playerSizer.Add(self._audaciousButton, 0, wx.TOP|wx.BOTTOM, 3)
+            self._playerSizer.Add(self._audaciousButton, 0, wx.TOP | wx.BOTTOM, 3)
 
             if self._settings["player"] == "Audacious":
                 self._iTunesButton.SetValue(True)
-                
+
             if not util.getIsInstalled("Audacious"):
                 self._iTunesButton.Enable(False)
 
         wx.EVT_RADIOBUTTON(self, ID_PLAYER, self._onPlayerChange)
-        
+
     def _onPlayerChange(self, e):
         if util.SYSTEM_NAME in util.WINDOWS_NAMES:
             if self._winampButton.GetValue():
                 self._settings["player"] = "Winamp"
             elif self._iTunesButton.GetValue():
                 self._settings["player"] = "iTunes"
-                
+
         elif util.SYSTEM_NAME in util.FREEBSD_NAMES:
             if self._xmmsButton.GetValue():
                 self._settings["player"] = "XMMS"
-                
+
         elif util.SYSTEM_NAME in util.MAC_NAMES:
             if self._iTunesButton.GetValue():
                 self._settings["player"] = "iTunes"
-        
+
         elif util.SYSTEM_NAME in util.LINUX_NAMES:
             if self._audaciousButton.GetValue():
                 self._settings["player"] = "Audacious"
-        
+
     def _setDefaults(self, safePlayers, defaultPlayer):
         self._safePlayers = safePlayers
         self._defaultPlayer = defaultPlayer
